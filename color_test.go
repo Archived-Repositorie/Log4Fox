a9f5ca26c7aa986d4f9e122cbd769592abbda1c2
JustFoxx <justfoxxo@outlook.com>
2022-03-04 16:08:17 +0100

    changted color handling

--- color_test.go
+++ color_test.go
@@ -3,7 +3,7 @@ package log4fox
 import "testing"
 
 func TestClr(t *testing.T) {
-	if Color().Font.BLACK != "\033[0;30m" {
+	if C.Font.BLACK != "\033[0;30m" {
 		t.Error("ColorReset: Invalid constant value")
 	}
 }

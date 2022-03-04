182ff45301df4b1c603ba3437dd24e368a3b3670
MCWertGaming <dleven@protonmail.com>
2022-03-04 20:57:58 +0100

    simplified the color management

--- color_test.go
+++ color_test.go
@@ -3,11 +3,11 @@ package log4fox
 import "testing"
 
 func TestClr(t *testing.T) {
-	if C.Font.BLACK != "\033[0;30m" {
+	if FONT_BLACK != "\033[0;30m" {
 		t.Error("ColorReset: Invalid constant value")
 	} else {
-		c := Color(C.Font.BLACK, "Hello world!")
-		
+		c := Color(FONT_BLACK, "Hello world!")
+
 		Stderr(c)
 	}
 }
--- coloring.go
+++ coloring.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func Color(c color, s string) string {
-	return string(c) + s + string(C.RESET)
+func Color(c string, s string) string {
+	return c + s + RESET
 }
 
-func Colorf(c color, s string, k ...interface{}) string {
-	return string(c) + fmt.Sprintf(s, k...) + string(C.RESET)
+func Colorf(c string, s string, k ...interface{}) string {
+	return c + fmt.Sprintf(s, k...) + RESET
 }
 
-func Colorln(c color, s ...interface{}) string {
-	return string(c) + fmt.Sprintln(s...) + string(C.RESET)
+func Colorln(c string, s ...interface{}) string {
+	return c + fmt.Sprintln(s...) + RESET
 }

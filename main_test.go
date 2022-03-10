348e0b34cda252c26f78a33cd73d16f17ce51d92
JustFoxx <justfoxxo@outlook.com>
2022-03-10 19:44:42 +0100

    changed place of tests

--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package log4fox
 
 import (
 	"testing"
+
+	"github.com/PurotoApp/Log4Fox/essential"
+	c "github.com/PurotoApp/Log4Fox/color"
 )
 
 var testLogger = loggerObject{Name: "test", Color: true}
@@ -9,5 +12,15 @@ var testLogger = loggerObject{Name: "test", Color: true}
 func TestLog(t *testing.T) {
 	if err := testLogger.Logln("Test"); err != nil {
 		t.Error(err)
+	}
+}
+
+func TestClr(t *testing.T) {
+	if c.FONT_BLACK != "\033[0;30m" {
+		t.Error("ColorReset: Invalid constant value")
+	} else {
+		c := c.Color(c.FONT_RED_UL, "Hello world!")
+
+		essential.Stderr(c)
 	}
 }

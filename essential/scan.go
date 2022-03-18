f5e97a7b77242da90279d13f8e50a7b3903bce7f
JustFoxx <justfoxxo@outlook.com>
2022-03-18 17:21:08 +0100

    Added scanning

--- essential/scan.go
+++ essential/scan.go
@@ -1,5 +1,23 @@
 package essential
 
 import (
-	_ "golang.org/x/sys/unix"
+	"bufio"
+	"os"
 )
+
+func ReaderStr(s *bufio.Scanner,r bool) string {
+	if r {
+		return s.Text()
+	} else {
+		return ""
+	}
+}
+
+func Reader(s *os.File) (bool, *bufio.Scanner) {
+	scan := bufio.NewScanner(s)
+	return scan.Scan(), scan
+}
+
+func ReaderErr(s *bufio.Scanner) error {
+	return s.Err()
+}

8370471da40a7bf87880cce0b5f81f664b19d642
JustFoxx <justfoxxo@outlook.com>
2022-03-24 20:08:23 +0100

    will add documention in near feature, not now

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
-//Library for easier managing logging stuff in GO
 package log4fox
 
-//Base interface for every logger 
 type Logger interface {
 	Log() error
 	Logf() error
 	Logln()	error
 }
 
-/*
-Base structure for every logger
-Using `Name` property you can change how name of app is shown 
-Using `DateFormat` property you can change how date is shown
-Using `Color` property you can change if you want use color or not
-*/
 type LoggerObject struct {
 	Name string
 	DateFormat string
